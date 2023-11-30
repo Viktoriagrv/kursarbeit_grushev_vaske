ab43c291e5fbe84138c90962e824ea7b449eedb2
Viktoria Grushev <126667727+Viktoriagrv@users.noreply.github.com>
2023-11-30 21:35:02 +0100

    Index Dateien für Viktoria und Lisa

---
+++ vaske_lisa/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meine Webseite</title>
+</head>
+<body>
+
+    <header>
+        <h1>Willkommen auf meiner Webseite!</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="#">Startseite</a></li>
+            <li><a href="#">Titel</a></li>
+            <li><a href="#">Kontakt</a></li>
+        </ul>
+    </nav>
+
+    <section>
+        <h2>Kursarbeit</h2>
+    </section>
+
+   
+
+</body>
+</html>
