82ff5e4c585d5c4b4c200e42ae6a526587e26926
Viktoria Grushev <126667727+Viktoriagrv@users.noreply.github.com>
2023-11-30 21:16:01 +0100

    Errichten von der index.php Seite

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hauptseite Kursarbeit</title>
+<link href="/css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	
+	<p> <a href="Vorlesungswoche 3/Aufgabe 1/formular.php">Fallzeitberechnung von Planeten</a></p>
+	<p> <a href="Vorlesungswoche 3/Aufgabe 2/formular.php">Tankpreis</a></p>
+	<p> <a href="Vorlesungswoche 3/Aufgabe 3/formular.php">Gleichung</a></p>
+	<p> <a href="Vorlesungswoche 3/Aufgabe 4/formular.php">Rechteck</a></p>
+	<p> <a href="Vorlesungswoche 3/Aufgabe 5/formular.php">BMI</a></p>
+	<p> <a href="Vorlesungswoche 3/Aufgabe 6/formular.php">Bandbreite</a></p>
+	<p> <a href="Vorlesungswoche 5/index.php">Übungen vom 26. Oktober 2023</a></p>
+	<br>
+	
+</body>
+	
+</html>
+
+
+<!doctype html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>JavaScript</title>
+
+</head>
+
+
+</html>
+
+	
+
+<p> <a href="lisavaske/index.php">Lisas Seite</a></p>
+	<p> <a href="viktoriagrushev/index.php">Viktorias Seite</a></p>
+
+</body>
+
+</html>
