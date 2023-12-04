f2b387776c18bc8ccea18dca6f01deb7987957ea
Viktoria Grushev <126667727+Viktoriagrv@users.noreply.github.com>
2023-12-04 12:20:18 +0100

    Test Bootstrap
    Testen und Installieren meiner Bootstrap Dateien

--- grushev_viktoria/index.php
+++ grushev_viktoria/index.php
@@ -1,29 +1,15 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edulearn:Lerntypen und Modulaufwand</title>
-</head>
-<body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Viktorias Seite</title>
+	  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+	  
+    <h1>Lerntyen und Modulaufwand</h1>
 
-    <header>
-        <h1>Willkommen auf meiner Webseite!</h1>
-    </header>
-
-    <nav>
-        <ul>
-            <li><a href="#">Startseite</a></li>
-            <li><a href="#">Titel</a></li>
-            <li><a href="#">Kontakt</a></li>
-        </ul>
-    </nav>
-
-    <section>
-        <h2>Kursarbeit</h2>
-    </section>
-
-   
-
-</body>
+	  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
 </html>
