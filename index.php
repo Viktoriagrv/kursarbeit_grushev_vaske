26a3b1ac42eea800a2bcc5dcc4d9c0ebc31e0ec1
Lisavaske <lisa.vaske@student.jade-hs.de>
2023-12-06 18:01:10 +0100

    Einrichtung

--- index.php
+++ index.php
@@ -38,8 +38,7 @@
 	    
 	 <!-- Hier fängt mein Footer an! --> 
 	    <footer>
-        Kursarbeit Lisa Vaske und Viktoria Grushev
-    </footer>
+        Kursarbeit Lisa Vaske und Viktoria Grushev</footer>
 	  
 <!-- Einbindung von Bootstrap Java Script -->
 	  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
