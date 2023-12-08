29ce92cc357ff049344212d78ff998873e9398e5
Lisavaske <lisa.vaske@student.jade-hs.de>
2023-12-08 19:29:32 +0100

    erste Bearbeitung Infoseite Lisa

--- vaske_lisa/InfoLisa.php
+++ vaske_lisa/InfoLisa.php
@@ -19,10 +19,89 @@
     </header>
 
     <main>
-        <h2>Willkommen auf Info-Seite</h2>
+        <h2>Willkommen auf der Info-Seite</h2>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
         <!-- Hier kommt der Hauptinhalt deiner Seite -->
     </main>
 
+	
+	
+	
+	              <div class="row justify-content-end">
+                        <div class="col-11 col-md-10 col-lg-6">
+                            <div class="cc-box block-color-blue">
+                                <div class="brabo-content">
+                                    class="color-initial"
+									<h3>Kognitive Lernstartegien</h3>
+	<p>Kognitive Lernstrategien umfassen die essenziellen Prozesse des Lernens: das Begreifen, das Auswendiglernen, das Üben, das Erinnern und das Anwenden des Gelernten. In manchen Fächern ist reines Auswendiglernen unumgänglich, während in anderen die Anwendung des Gelernten von größerer Bedeutung ist. Alles kann mithilfe passender Methoden erlernt werden. Hier sind einige der bedeutendsten kognitiven Lernstrategien:
+
+	<section><b>Auswendig lernen:</b></section>
+
+Bestimmte Informationen müssen einfach auswendig gelernt werden. Das betrifft beispielsweise Vokabeln oder spezifische Begriffe, deren Benennung erlernt werden muss. Städtenamen, medizinische Termini, Paragraphen – die Palette ist vielfältig. Wiederholen, lautes Wiederholen und das Leitner-System mit Karteikarten sind effektive Methoden, um Informationen zu memorisieren.
+
+<section><b>Verständnis:</b></section>
+
+Die Herausforderung liegt im Verstehen. Je abstrakter eine Theorie ist, desto komplizierter kann es sein, sie zu begreifen. Hier helfen Lernstrategien wie Mindmaps, das Zusammenfassen von Texten, visuelle Darstellungen und der Vergleich mit ähnlichen Themenbereichen. Aktive Teilnahme am Unterricht, das Stellen von Fragen und der Austausch mit Mitschülern oder Kommilitonen können das Verständnis fördern.
+
+<section><b>Anwendung:</b></section>
+
+Das Hauptziel des Lernens ist die praktische Anwendung des Gelernten. Hierbei ist Übung der Schlüssel zum Erfolg. Wer die Begriffe kennt und das Thema verstanden hat, muss intensiv üben – am besten mit einer Vielzahl unterschiedlicher Beispiele.</p>
+	
+	<img scr="bilder/kognitiv.jpg" alt="kognitive Lernstrategie">
+	
+	
+	<h4>Metakognitive Lernstartegien</h4>
+<p><br>Metakognitive Lernstrategien konzentrieren sich auf die Planung und effiziente Nutzung deiner Lernzeit. Eine der bekanntesten Strategien ist:</br>
+
+<br><b>Farbkodierung:</b></br>
+
+<br>Diese Methode, auch als Farbleitsystem bekannt, ist besonders vorteilhaft für Personen mit einem visuellen Gedächtnis. Jedes Thema oder Fach wird einer bestimmten Farbe zugeordnet, was sowohl beim Zeitmanagement als auch beim eigentlichen Lernprozess hilft.</br>
+
+<br><b>Zeitplanung:</b></br>
+
+<br>Timeboxing, eine Methode, die auch in der Arbeitswelt weit verbreitet ist, weist jedem Task einen bestimmten Zeitrahmen zu. Diese strukturierte Herangehensweise reduziert Stress und gewährleistet, dass jedem Thema ausreichend Aufmerksamkeit geschenkt wird.</br>
+
+<br><b>Pomodoro-Technik:</b></br>
+
+<br>Eine weitere beliebte Methode ist die Pomodoro-Technik, bei der du deine Lernzeit mit einer Eieruhr oder Timer festlegst. Produktives Lernen wird durch regelmäßige Pausen abgewechselt, was die Konzentration und Effektivität steigern kann.</p></br>
+	
+<img scr="bilder/metakognitiv.jpg" alt="Metakognitive Lernstrategie">	
+
+
+<p><b>Ressourcenbezogene Lernstrategien</b></p>
+<br>Ressourcenbezogene Lernstrategien zielen darauf ab, das Lernen angenehmer zu gestalten. Dies steigert die Motivation, was sich wiederum positiv auf die Ergebnisse auswirkt. Allerdings erfordern diese Strategien oft Ressourcen wie Geld oder Zeit.</br>
+
+<br><b>Ästhetische Materialien und ein angenehmer Lernplatz:</b></br>
+
+<br>Ähnlich wie wir motivierter ins Fitnessstudio gehen, wenn wir in Sportkleidung sind, macht das Lernen an einem ordentlichen und ansprechenden Schreibtisch mehr Spaß. Schöne Stifte und ansprechende Notizen können die Motivation steigern.</br>
+
+<br><b>Gemeinsames Lernen mit Freunden:</b></br>
+
+<br>Obwohl es auf den ersten Blick nicht besonders produktiv erscheinen mag, steigert das Lernen mit Freunden die Motivation. Gegenseitige Unterstützung und ein Hauch von Wettbewerb können motivierend wirken. Letztlich sollte das Lernen auch Spaß machen, und das gelingt oft gut in Gesellschaft von Freunden.</br>
+
+<br><b>Ein ausgewogenes Leben:</b></br>
+
+<br>Ausreichend Schlaf, eine gesunde Ernährung und ein ausgeglichenes Privatleben sind entscheidend für erfolgreiches Lernen. Um in der Schule oder Universität erfolgreich zu sein, ist es wichtig, für das eigene Wohlbefinden zu sorgen. Es mag vielleicht spirituell klingen, aber ohne Pausen oder Ausgleich kann das Gehirn Gelerntes nicht verarbeiten. Daher ist es wichtig, auf ausreichenden Schlaf zu achten und entspannende Hobbys zu finden.</br>
+
+<img scr="bilder/ressourcenbezogen.jpg" alt="Ressourcenbezogene Lernstartegie">
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
