d469d7b2f67a680731c8ee5c2a3bf6430b7322c3
Viktoria Grushev <126667727+Viktoriagrv@users.noreply.github.com>
2023-12-16 15:39:13 +0100

    Bootseinbau Navigation Lisa

--- vaske_lisa/InfoLisa/Lernstartegien.php
+++ vaske_lisa/InfoLisa/Lernstartegien.php
@@ -3,6 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <title>LernenVerstehen</title>
+	<!-- Definition vom Dokumententypen-->
+<!doctype html>
+<!-- Definition der Sprache "de" = deutsch -->
+<html lang="de">
+	
+	<!-- Meta-Daten sind im Headbereich anzutreffen-->
+  <head>
+	  
+	  <!-- Zeichensatz UTF-8 -->
+    <meta charset="utf-8">
+	  
+	  <!-- Anzeigeeingenschaften für Geräte -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	  
+    <title> Erfassung der Lerntypen </title>
+	
+	<!-- Bootstrap-Einbindung -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
 	
 	<!-- Verknüpfung zur CSS-Datei -->
     <link rel="stylesheet" href="styles.css"> 
