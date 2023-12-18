c4308000c905b4eb7a25d4c08559f50ba4aadb0c
Viktoria Grushev <126667727+Viktoriagrv@users.noreply.github.com>
2023-12-18 20:01:29 +0100

    Statsitik Kreisdiagram

--- grushev_viktoria/Statistiken/statistiklerntyp.php
+++ grushev_viktoria/Statistiken/statistiklerntyp.php
@@ -1,92 +1,46 @@
-<!-- Definition vom Dokumententypen-->
-<!doctype html>
-<!-- Definition der Sprache "de" = deutsch -->
+<!DOCTYPE html>
 <html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
-	<!-- Meta-Daten sind im Headbereich anzutreffen-->
-  <head>
-	  
-	  <!-- Zeichensatz UTF-8 -->
-    <meta charset="utf-8">
-	  
-	  <!-- Anzeigeeingenschaften für Geräte -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	  
-    <title> Statistik </title>
-	
-	<!-- Bootstrap-Einbindung -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
+    <title>Dynamisches Kuchendiagramm</title>
+    
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
 
-	
-  <body>
-	  
-    <!-- Überschrift -->
-	<h1> Erfassung deines Lerntypen </h1>
-	  
-	  <!-- Navigationsleiste mit Bootstrap -->
-	  
-	  <ul class="nav nav-tabs">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Home</a>
-  </li>
-		  
-  <!-- Dropdownlink für die Informationen mit Bootstrap -->
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Informationen</a>
-    <ul class="dropdown-menu">
-		  <!-- Verlinkung für die einzelnen Seiten -->
-      <li><a class="dropdown-item" href="Informationsseite/info_visuell.php">Lerntyp Visuell </a></li>
-            <li><a class="dropdown-item" href="Informationsseite/info_auditiv.php"> Lerntyp Auditiv </a></li>
-                 <li><a class="dropdown-item" href="Informationsseite/info_motorisch.php"> Lerntyp Motorisch </a></li>
-		             <li><a class="dropdown-item" href="Informationsseite/info_kommunikativ.php"> Lerntyp Kommunikativ</a></li>
-		  </ul>     
-		  
-      <!-- Dropdownlink 2 für die Erfassungsseite mit der Datenbankverindung! Vielleicht kommt das auf die Startseite -->
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Erfassung deiner Daten</a>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="#">Lertyp Datenerfassung</a></li>
-    </ul> 
-	  
-	  <!-- Dropdown Link 3 für die Statistikseiten. Geplant: 2 --> 
-	  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Statistiken</a>
-    <ul class="dropdown-menu">
-	    <!-- Verlinkung Statistik -->
-                <li><a class="dropdown-item" href="Statistiken/statistiklerntyp.php">Statistik Lerntypen </a></li>
-                        
-                                                                                     </ul> 
-	  
-	   <!-- Link zur Startseite -->
-		                       <li class="nav-item">
-                                          <a class="nav-link" href="../index.php">Zurück zur Startseite</a>
-  </li>
-</ul>
-	  
+        function drawChart() {
+            // Daten vom Server mit AJAX abrufen
+            $.ajax({
+                url: 'statistik_erfassung.php', // Verlinkung PHP Datei
+                dataType: 'json',
+                success: function (data) {
+                    var chartData = new google.visualization.DataTable();
+                    chartData.addColumn('string', 'Lerntyp');
+                    chartData.addColumn('number', 'Anzahl');
 
-</div>
-<!-- Beginn von Formular -->
-<div class="container-fluid">
-    <div class="mt-5 mb-5 row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-         <h1> Statistik</h1>
-			
-			
-			</form>
-        </div>
-    </div>
-</div>
-	  			
-	
-<
-   <!-- Bootstrap Footer -->
-    <footer class="fixed-bottom bg-primary text-center p-2">
-      <p class="text-light mb-0">IT2 Kursarbeit von Viktoria Grushev und Lisa Vaske</p>
-    </footer>
-<!-- Einbindung von Bootstrap Java Script -->
-	  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-	 
-  </body>
+                    // Annahme: Deine Daten haben 'lerntyp' und 'lerntyp_id' Spalten
+                    for (var i = 0; i < data.length; i++) {
+                        chartData.addRow([data[i].lerntyp, parseInt(data[i].anzahl)]);
+                    }
+
+                    var options = {
+                        'title': 'Lerntypen Verteilung',
+                        'width': 400,
+                        'height': 300
+                    };
+
+                    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+                    chart.draw(chartData, options);
+                }
+            });
+        }
+    </script>
+</head>
+<body>
+    <div id="chart_div"></div>
+</body>
 </html>
