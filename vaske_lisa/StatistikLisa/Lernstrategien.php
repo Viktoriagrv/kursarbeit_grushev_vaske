60558d8e08fa05fca5fa02d30b28d05b958a135a
Lisavaske <lisa.vaske@student.jade-hs.de>
2023-12-18 23:22:58 +0100

    Statistiken

--- vaske_lisa/StatistikLisa/Lernstrategien.php
+++ vaske_lisa/StatistikLisa/Lernstrategien.php
@@ -23,7 +23,20 @@
     }
   </style>	
 	
+
 	
+	
+	
+	
+<style>
+	/*CSS-Styling für die Kuchendiagramme, Abstände */
+	.chart-container {
+	display:flex;
+	}
+	.chart-item {
+	margin-right: 20px; 
+	
+</style>
 	
 	
 	
@@ -126,168 +139,103 @@
 	
     <main>
         <h2>Willkommen auf der Statistik-Seite</h2>
-        <!-- Hier kommt der Hauptinhalt deiner Seite -->
+        <!-- Hier kommt der Hauptinhalt meiner Seite -->
     </main>
 
 
 
-<!-- Google Charts Kuchendiagramm-->
+
 
 <html>
   <head>
-    <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-
-      // Load the Visualization API and the corechart package.
       google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawCharts);
 
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
+      function drawCharts() {
+		  
+		  
+		  
+        // Daten für das kognitive Diagramm
+        var data1 = new google.visualization.DataTable();
+        data1.addColumn('string', 'Topping');
+        data1.addColumn('number', 'Slices');
+        data1.addRows([
           ['gelungen', 2],
           ['mäßig gelungen', 2],
           ['nicht gelungen', 1]
-          
         ]);
+        var options1 = {
+          'title':'Lernerfolg per kognitiver Lernstrategie',
+          'width':400,
+          'height':300
+        };
+        var chart1 = new google.visualization.PieChart(document.getElementById('chart_div_1'));
+        chart1.draw(data1, options1);
 
-        // Set chart options
-        var options = {'title':'Lernerfolg per kognitiver Lernstrategie',
-                       'width':400,
-                       'height':300};
-
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-    </script>
-  </head>
-
-  <body>
-    <!--Div that will hold the pie chart-->
-    <div id="chart_div"></div>
-  </body>
-</html>
-
-<!-- kognitive Lernstrategie zuende-->
-
-
-
-
-
-<html>
-  <head>
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-
-      // Load the Visualization API and the corechart package.
-      google.charts.load('current', {'packages':['corechart']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
+		  
+		  
+		  
+        // Daten für das metakognitive Diagramm
+        var data2 = new google.visualization.DataTable();
+        data2.addColumn('string', 'Topping');
+        data2.addColumn('number', 'Slices');
+        data2.addRows([
           ['gelungen', 1],
           ['nicht gelungen', 4]
-          
         ]);
+        var options2 = {
+          'title':'Lernerfolg per metakognitiver Lernstrategie',
+          'width':400,
+          'height':300
+        };
+        var chart2 = new google.visualization.PieChart(document.getElementById('chart_div_2'));
+        chart2.draw(data2, options2);
 
-        // Set chart options
-        var options = {'title':'Lernerfolg per metakognitiver Lernstrategie',
-                       'width':400,
-                       'height':300};
-
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-    </script>
-  </head>
-
-  <body>
-    <!--Div that will hold the pie chart-->
-    <div id="chart_div"></div>
-  </body>
-</html>
-
-<!-- metakognitive lernstartegie zuende-->
-
-
-
-
-
-
-
-
-<html>
-  <head>
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-
-      // Load the Visualization API and the corechart package.
-      google.charts.load('current', {'packages':['corechart']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
+		  
+		  
+		  
+        // Daten für das ressourcenbezogene Diagramm
+        var data3 = new google.visualization.DataTable();
+        data3.addColumn('string', 'Topping');
+        data3.addColumn('number', 'Slices');
+        data3.addRows([
           ['gelungen', 2],
           ['mäßig gelungen', 1],
           ['nicht gelungen', 1]
-          
         ]);
-
-        // Set chart options
-        var options = {'title':'Lernerfolg per ressourcenbezogener Lernstrategie',
-                       'width':400,
-                       'height':300};
-
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
+        var options3 = {
+          'title':'Lernerfolg per ressourcenbezogener Lernstrategie',
+          'width':400,
+          'height':300
+        };
+        var chart3 = new google.visualization.PieChart(document.getElementById('chart_div_3'));
+        chart3.draw(data3, options3);
       }
     </script>
+
+   
   </head>
 
   <body>
-    <!--Div that will hold the pie chart-->
-    <div id="chart_div"></div>
+	  
+	  
+	  
+    <!-- Container für die Kuchendiagramme -->
+    <div class="chart-container">
+      <div class="chart-item">
+        <div id="chart_div_1"></div>
+      </div>
+      <div class="chart-item">
+        <div id="chart_div_2"></div>
+      </div>
+      <div class="chart-item">
+        <div id="chart_div_3"></div>
+      </div>
+    </div>
   </body>
 </html>
-
-<!-- ressourcenbezogene Lernstartegie zuende-->
-
-
 
 
 
