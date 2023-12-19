d771777e35e12a14d16ea41124f01d66ce6b362d
Viktoria Grushev <126667727+Viktoriagrv@users.noreply.github.com>
2023-12-19 18:20:21 +0100

    Footer und Änderung

--- index.php
+++ index.php
@@ -235,8 +235,8 @@ mysqli_close($con1);
 </div>
 
    <!-- Bootstrap Footer -->
-    <footer class="fixed-bottom bg-primary text-center p-2">
-      <p class="text-light mb-0">IT2 Kursarbeit von Viktoria Grushev und Lisa Vaske</p>
+  <footer class="bg-primary text-center p-2">
+        <p class="text-light mb-0">IT2 Kursarbeit von Viktoria Grushev und Lisa Vaske</p>
     </footer>
 			
 <!-- Einbindung von Bootstrap JavaScript -->
