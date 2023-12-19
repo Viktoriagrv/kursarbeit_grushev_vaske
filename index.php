f48d87467dd7ea67ff274fb5752e5d4d9eadab75
Viktoria Grushev <126667727+Viktoriagrv@users.noreply.github.com>
2023-12-19 22:33:33 +0100

    CSS Datei Saubermachen

--- index.php
+++ index.php
@@ -7,23 +7,18 @@
     <title>Startseite Kursarbeit</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-	
-<style>
-    /* CSS-Styling für das Logo */
-    #logo {
-      position: absolute;
-      top: 1px; 
-      right: 10px; 
-    }
-  </style>	
 	
 	
 	</head>
 <body>
 <header>
-    <h1>EduLearn</h1>
-
+	  <div>
+        <h1>EduLearn</h1>
+    </div>
+    <div>
+        <img src="vaske_lisa/bilder/Logo_rund1.png" alt="Logo" class="logo">
+    </div>
+    
 </header>
 
 
@@ -50,8 +45,6 @@
 	<br>
 	
 <div>	
-	
-<img id="logo" src="vaske_lisa/bilder/Logo_rund1.png" alt="UnserLogo" width="100" height="100">	
 	
 </div>
 	</br>
