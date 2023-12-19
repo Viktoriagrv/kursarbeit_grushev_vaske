08367223f0d4bd670aafad52de1c80ea297c386e
Lisavaske <lisa.vaske@student.jade-hs.de>
2023-12-19 22:12:03 +0100

    CSS

--- index.php
+++ index.php
@@ -9,15 +9,7 @@
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 	
-<style>
-    /* CSS-Styling für das Logo */
-    #logo {
-      position: absolute;
-      top: 1px; 
-      right: 10px; 
-    }
-  </style>	
-	
+
 	
 	</head>
 <body>
