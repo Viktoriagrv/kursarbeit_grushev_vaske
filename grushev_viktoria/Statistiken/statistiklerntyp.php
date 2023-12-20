a87a35f4d9c69dee724454faece543d3f4fb3a7e
Viktoria Grushev <126667727+Viktoriagrv@users.noreply.github.com>
2023-12-20 15:43:01 +0100

    Veränderung!

--- grushev_viktoria/Statistiken/statistiklerntyp.php
+++ grushev_viktoria/Statistiken/statistiklerntyp.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	 <link rel="stylesheet" href="../css/grushev_css/grushev_css.css"> 
-    <title>Statistik Lerntypen</title>
+	<link rel="stylesheet" href="../css/grushev_css/grushev_css.css"> 
+    <title>Statistik Lerntyp</title>
     	
 	<!-- Bootstrap-Einbindung -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -29,11 +29,12 @@
 
                     // Annahme: Deine Daten haben 'lerntyp' und 'lerntyp_id' Spalten
                     for (var i = 0; i < data.length; i++) {
-                        chartData.addRow([data[i].lerntyp, parseInt(data[i].anzahl)]);
+                    chartData.addRow([data[i].lerntyp, parseInt(data[i].anzahl)]);
                     }
-
+ 
+					// Größeneinstellung 
                     var options = {
-                        'title': 'Lerntypen Verteilung',
+                        'title': 'Lerntyp Verteilung',
                         'width': 600,
                         'height': 400
                     };
@@ -44,13 +45,13 @@
             });
         }
     </script>
-</head>
+	
 	<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-fXxu7mz9ATKxIepV6+rnNS3EKlkNw0dbV4N+s3VNA6Z/rU6w+Yvdy4ZI6A0CnC5u" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-fXxu7mz9ATKxIepV6+rnNS3EKlkNw0dbV4N+s3VNA6Z/rU6w+Yvdy4ZI6A0CnC5u" crossorigin="anonymous"></script>
 
-<!--Beginn Body -->
+</head>
 <body>
-	<!-- Header -->
+     <!-- Header -->
 	  <header>
 	     <div>
             <h1>EduLearn</h1>
@@ -61,12 +62,13 @@
                                                                                                </div>
                                                                                                  </header>
 
-	                                   <!-- Navigationsleiste mit Bootstrap -->
+	  
+		   <!-- Navigationsleiste mit Bootstrap -->
 	  
 	                                          <ul class="nav nav-tabs">
                                                   <li class="nav-item">
 													  <!--Verlinkung zur Homestartseite von mir -->
-                                                      <a class="nav-link active" aria-current="page" href="lerntyp.erfassung.php">Home</a>
+                                                      <a class="nav-link active" aria-current="page" href="../../lerntyp_erfassung.php">Home</a>
                                                                     </li>
 		  
   <!-- Dropdownlink 1 Bootstrap: Informationen -->
@@ -85,10 +87,10 @@
                             <li class="nav-item dropdown">
                                   <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Erfassung deiner  Daten</a>
                                              <ul class="dropdown-menu">
-                                                  <li><a class="dropdown-item" href="../../lerntyp_erfassung.php"> Generelles Erfassungsformular </a></li>
+                                                  <li><a class="dropdown-item" href="../../grunderfassung_lerntyp.php"> Generelles Erfassungsformular </a></li>
                                                         <li>
-												         	 <a class="dropdown-item" href="lerntyp.seite.php"> Neues Lerntyp-Profil anlegen</a> </li>
-		                                                          <li><a class="dropdown-item" href="../modulerfassung/modul.seite.php"> Neues Modul-Profil anlegen</a></li>
+												         	 <a class="dropdown-item" href="../../Erfassungsseite/lerntyperfassung/lerntyp.seite.php"> Neues Lerntyp-Profil anlegen</a> </li>
+		                                                          <li><a class="dropdown-item" href="../../Erfassungsseite/modulerfassung/modul.seite.php"> Neues Modul-Profil anlegen</a></li>
 						                                                                </ul> 
 						  
 	  
@@ -97,9 +99,9 @@
                         <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Statistiken</a>
                                         <ul class="dropdown-menu">
 										
-                       <li> <a class="dropdown-item" href="../../Statistiken/statistiklerntyp.php">Statistik Lerntypen </a></li>
-	                        <li><a class="dropdown-item" href="../../Statistiken/modulstatistik/statistikmodul.php">Statistik Modul </a></li>
-							  <li><a class="dropdown-item" href="../../Statistiken/generalstatistik/generalstatistik.php"> Generelle Statistik </a></li>
+                       <li> <a class="dropdown-item" href="../statistiklerntyp.php">Statistik Lerntypen </a></li>
+	                        <li><a class="dropdown-item" href="statistikmodul.php">Statistik Modul </a></li>
+							  <li><a class="dropdown-item" href="../generalstatistik/generalstatistik.php"> Generelle Statistik </a></li>
                                                                                                                                               </ul> 
 	  
 	                              <!-- Link zur gemeinsamen Startseite -->
@@ -109,38 +111,26 @@
 	                                               </ul>
                                                       </div>
 		<!-- Navigationsleiste zuende! -->
-	 
-		  <!-- Beginn von Inhalt -->
-		    <!-- Bootstrap Grid-Layout -->
-                <div class="container-fluid">
-                      <div class="mt-5 mb-5 row">
-                        <div class="col-md-4"></div>
-                           <div class="col-md-4">
-							   
-            <!-- Formular -->
-                 <!-- Selbstgemachtes Bildeinfügen -->
-			         <img src="../../Bilder/Lerntyp.png" class="img-fluid" alt="...">
-							   <!-- Bei Post an lerntyp.erfassung.php weiterleiten -->
-                                       <form action="lerntyp.erfassung.php" method="post">
-					<!-- Überschrifft -->					   
-            <h4> Erfasse ein neues Lerntyp-Profil</h4>
-				<!-- Blauer Text für neue Seite in Blocksatz und kursiver Schrift -->
-<p class="blue-text"> Natürlich probieren wir so viele Lerntypen wie möglich vorzustellen,
-				    manchmal hat man aber auch ganz bestimmte Kombinationen... Füge sie hinzu, vielleicht haben auch andere Student_innen deine Lerntyp-Kombi! </p>		
-               
-										   	<p class="blue-text"> (PS: Wenn du deinen Lerntypen später im Formular auswählst, wird dieser in unserer Statistik angezeigt!) </p>		
-										   
-       <p class="custom-form-group"><input name="lerntyp"> Dein Lerntyp</p>
-						  
-                <!-- Absenden -->
-                <button type="submit" class="mb-5 btn btn-primary" name="gesendet">Senden</button>
-   
-				<br>
+	  
+<!-- Beginn von Statistik in der Mitte -->
+<div class="container-fluid">
+    <div class="mt-5 mb-5 row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+			<div id="chart_div"></div>
+			<p class="blue-form"> Welches Modul wird am meisten unter Studenten hier gelernt? Hier kriegt ihr immer eine aktuelle Vorschau! </p>
+  <!-- Link zur anderen Statistik -->
+<a href="class="btn btn-secondary mb-5">Willst du sehen wie es bei den Lerntypen aussieht, die gelernt werden? Hier entlang!</a>
+	  
 
-				<!-- Link zu Modulerfassung -->
-<a href="../../lerntyp_erfassung.php"class="btn btn-secondary mb-5">Anders überlegt? Dann zurück zur Erfassungsseite!</a>
+</div>
+<!-- Beginn von Statistik -->
+<div class="container-fluid">
+    <div class="mt-5 mb-5 row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+    <div id="chart_div"></div>
 			
-		
 </form>
         </div>
     </div>
@@ -153,6 +143,8 @@
       <p class="text-light mb-0">IT2 Kursarbeit von Viktoria Grushev und Lisa Vaske</p>
     </footer>
 
-			
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+	
 </body>
 </html>
+
